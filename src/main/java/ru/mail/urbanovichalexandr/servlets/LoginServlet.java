67575cf4b14bb824ce38240d8659135f15023f0a
Александр <Urbanovichalexandr@mail.ru>
2018-01-16 12:18:17 +0200

    no message

--- src/main/java/ru/mail/urbanovichalexandr/servlets/LoginServlet.java
+++ src/main/java/ru/mail/urbanovichalexandr/servlets/LoginServlet.java
@@ -6,13 +6,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet("/UrlShortener")
-public class UrlShortener extends HttpServlet {
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
 
-    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        request.getRequestDispatcher("WEB-INF/views/home.jsp").forward(request, response);
+        PrintWriter writer = response.getWriter();
+        writer.println("Metod Get from Login");
     }
-
 }
